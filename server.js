829f182591b850de73e9df6564f6af5a9488ddbb
maxsage <max.sage@torbay.gov.uk>
2018-08-13 15:58:36 +0100

    update

--- server.js
+++ server.js
@@ -2,8 +2,20 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
-app = express();
+
+const history = require('connect-history-api-fallback');
+var app = express();
+
 app.use(serveStatic(__dirname + "/dist"));
+app.use(history())
+app.use(serveStatic(__dirname + "/dist"));
+
+app.get('/*', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
 var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started '+ port);
+app.listen(port, function () {
+  console.log('server started '+ port);
+});
+
--- src/main.js
+++ src/main.js
@@ -19,10 +19,10 @@ const router = new VueRouter({
   }
 })
 
-router.beforeEach((to, from, next) => {
-  console.log('global beforeEach')
-  next()
-})
+// router.beforeEach((to, from, next) => {
+//   console.log('global beforeEach')
+//   next()
+// })
 
 /* eslint-disable no-new */
 new Vue({
